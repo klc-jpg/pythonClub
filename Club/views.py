4f7815c76fb8c3ed86d2788802f07e6ce7ba5afd
klc-jpg <72785327+klc-jpg@users.noreply.github.com>
2022-05-26 14:42:26 -0700

    resource view commit

--- Club/views.py
+++ Club/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from Club.models import Meeting, MeetingMinutes, Resource, Event 
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
+
+def resources_view(request):
+    resource_list=Resource.objects.all()
+    return render(request, "Club/resources.html", {'resource_list': resource_list})
+
+
+
 
 
 
