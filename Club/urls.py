41ec40c75d6e006fa75f76c020ba99425335f78c
klc-jpg <72785327+klc-jpg@users.noreply.github.com>
2022-05-30 23:28:26 -0700

    meeting and detail commit

--- Club/urls.py
+++ Club/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
    path('', views.index, name='index'),
-   path('resources/', views.resources_view, name='resources')
+   path('resources/', views.resources_view, name='resources'),
+   path('meetings/', views.meetings_view, name='meetings'),
+   path('meeting_minutes/<int:id>/', views.meeting_detail, name='details'),
 ]
--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from Club.models import Meeting, MeetingMinutes, Resource, Event 
+from contextlib import contextmanager
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting, MeetingMinutes, Resource, Event 
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -7,8 +9,24 @@ def index(request):
 
 def resources_view(request):
     resource_list=Resource.objects.all()
-    return render(request, "Club/resources.html", {'resource_list': resource_list})
+    return render(request, 'Club/resources.html', {'resource_list': resource_list})
 
+def meetings_view(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'Club/meetings.html', {'meeting_list': meeting_list})
+
+#def meeting_detail(request, id):
+    #details=get_object_or_404(Meeting, pk=id)
+    #return render(request, 'Club/meeting_minutes.html', {'details': details})
+
+def meeting_detail(request, id):
+    details=get_object_or_404(Meeting, pk=id)
+    attendance=MeetingMinutes.objects.only("meetingid", "minutestext").only("attendance")
+    context={
+        'details': details,
+        'attendance': attendance,
+    }
+    return render(request, 'Club/meeting_minutes.html',context=context)
 
 
 
